6e0496a1c2ef2ed5bf19d9ebd6ce96fb30890244
mehedi-h <mehedihs2015@gmail.com>
2023-08-06 21:52:59 +0600

    problem solving task

---
+++ moduleTask.js
@@ -0,0 +1,14 @@
+/*
+1. 19, 53 odd number
+2. Feet to inch
+    function name will be ---feetToInch---
+3. Centimeter to Meter
+    function name will be ---centimeterToMeter
+4. count paper
+    book1 > 100, book2 > 200, nook3 > 300
+    paper requirment
+5. ['sajid', 'mamun', 'kamal', 'Jhankar Mahabub', 'hasan mahmud']
+    best friend;
+6. [45, 68, 89, 25, 39, -61, 77, 24]
+    only positive
+*/
