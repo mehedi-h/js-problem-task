c1ab5e2155476a693f5e8f93c8aef6372974ba68
mehedi-h <mehedihs2015@gmail.com>
2023-08-06 13:46:55 +0600

    modified code

--- nastaPrice.js
+++ nastaPrice.js
@@ -3,14 +3,18 @@
 //No-3: If the number is divisible by 5 then show Poncho Pandob! insted of number;
 //No-4 : if the number is divisible by both 3 & 5 then show Hall in Fame! insted of number;
 
-function name(singaraQuantity, somucaQuantity, jilapiQuantity) {
+function nasta (singaraQuantity, somucaQuantity, jilapiQuantity) {
     const singaraPrice = 7;
     const somucaPrice = 10;
     const jilapiPrice = 15;
 
-    const totalSingaraPrice = singaraQuantity * singaraPrice;
-    const totalSomucaPrice = somucaQuantity * somucaPrice;
-    const totalJilapiPrice = jilapiQuantity * jilapiPrice;
+        const totalSingaraPrice = (singaraQuantity * singaraPrice);
+        const totalSomucaPrice = (somucaQuantity * somucaPrice);
+        const totalJilapiPrice = (jilapiQuantity * jilapiPrice);
 
-    const totalPrice = 
+        const totalPrice = totalSingaraPrice + totalSomucaPrice + totalJilapiPrice;
+    return totalPrice;
 }
+
+const totalPrice = nasta(5,3,2);
+console.log('Mama Singara, Somuca O Jilapir bill', totalPrice,'taka den.');
