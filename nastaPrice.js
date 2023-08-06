1e3c3cdd54cf3483c5ee57849e552692bfa6c908
mehedi-h <mehedihs2015@gmail.com>
2023-08-06 13:03:16 +0600

    find price verious nasta  items by function

---
+++ nastaPrice.js
@@ -0,0 +1,8 @@
+//No-1: Show 1-50;
+//No-2: If the number is divisible by 3 then show Hattrick! insted of number;
+//No-3: If the number is divisible by 5 then show Poncho Pandob! insted of number;
+//No-4 : if the number is divisible by both 3 & 5 then show Hall in Fame! insted of number;
+
+function name(params) {
+    
+}
