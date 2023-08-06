077686d94912db1b4d0c95710c89dcee63a803eb
mehedi-h <mehedihs2015@gmail.com>
2023-08-06 13:17:14 +0600

    modified

--- nastaPrice.js
+++ nastaPrice.js
@@ -3,6 +3,14 @@
 //No-3: If the number is divisible by 5 then show Poncho Pandob! insted of number;
 //No-4 : if the number is divisible by both 3 & 5 then show Hall in Fame! insted of number;
 
-function name(params) {
-    
+function name(singaraQuantity, somucaQuantity, jilapiQuantity) {
+    const singaraPrice = 7;
+    const somucaPrice = 10;
+    const jilapiPrice = 15;
+
+    const totalSingaraPrice = singaraQuantity * singaraPrice;
+    const totalSomucaPrice = somucaQuantity * somucaPrice;
+    const totalJilapiPrice = jilapiQuantity * jilapiPrice;
+
+    const totalPrice = 
 }
